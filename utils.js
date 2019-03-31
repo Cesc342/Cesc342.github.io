6540723345fa84f3343ecae60dd8f5bd16509841
Cesc342 <45318891+Cesc342@users.noreply.github.com>
2019-03-31 18:49:05 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -6,8 +6,21 @@ const Math = new function() {
   this.ep = function (m,g,h) {
     return g*h*m;
   }
+  this.densitat = function(m,v){
+    return m/v;
+  }
+  this.vel = function(d,t){
+    return d/t;
+  }
   this.c = new function() {
     this.num = 299792458;
     this.unitat = "m/s"
+  }
+  this.transform = function(valor,unitat,unitatPerTransformar){
+    if(unitat == "m/s" && unitatPerTransformar == "km/h"){
+       raturn valor * 3.6;
+    }else if(unitat == "km/h" && unitatPerTransformar == "km/h"){
+      return valor * 0.2777777777777778;
+    }
   }
 }
