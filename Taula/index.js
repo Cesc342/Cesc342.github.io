7f6484664d282e807946aae277bb1c4827614673
Cesc342 <45318891+Cesc342@users.noreply.github.com>
2020-02-01 18:07:25 +0100

    Add files via upload

--- Taula/index.js
+++ Taula/index.js
@@ -1,9 +1,18 @@
+n = 0;
+doc = document.getElementById(data[0][0]);
+
 document.addEventListener("keypress",(e)=>{
     i = 0;
     trobat = false;
     while(i<data[0].length && !trobat){
         if(e.key == data[0][i]){
-            data[1][i].play()
+            doc.style.border = "5px outset rgb(29, 29, 29)";
+            doc.style.backgroundColor = "rgb(90, 90, 90)";
+            doc = document.getElementById(data[0][i]);
+            doc.style.border = "5px inset rgb(29, 29, 29)";
+            doc.style.backgroundColor = "rgb(60, 60, 60)";
+            data[1][i].play();
+            n = i;
             trobat = true;
         }
         i++;
